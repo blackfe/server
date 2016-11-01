d5b226b758ffc23e8c7b454d1fa187ba5fe644cf
zhiqizhang <zhiqizhang@moonton.com>
2016-11-01 16:22:06 +0800

    remove module

--- common/config/serverDBBase.lua
+++ common/config/serverDBBase.lua
@@ -1,5 +1,7 @@
-module("Table")
-ServerList = {
+if Table == nil then
+	Table = {}
+end
+Table.ServerList = {
 __meta = {iID = 1,sName = 2},
 	["1001"] = {1001,"release"},
 	["1002"] = {1002,"develop"},
